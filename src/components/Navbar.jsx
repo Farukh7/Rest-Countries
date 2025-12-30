6571fa6c47c88583a9618d6416ee0872af1410d2
farukh7 <frk10906@gmail.com>
2025-12-30 17:53:19 +0530

    Add Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,20 @@
+import { Button } from "@mui/material";
+import BedtimeIcon from "@mui/icons-material/Bedtime";
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <div className="flex flex-row justify-between align-center shadow-xl p-8">
+        <h1 className="text-black p-1">
+          <strong>Where in the world?</strong>
+        </h1>
+        <Button color="inherit">
+          <BedtimeIcon />
+          Dark Mode
+        </Button>
+      </div>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
