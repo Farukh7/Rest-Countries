d2ef9464f231553e4c35bde1f4b037ee6eeb7673
farukh7 <frk10906@gmail.com>
2025-12-31 12:13:16 +0530

    Add Filter Region

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,38 +1,115 @@
-import Search from '../components/Search'
-import Countries from '../components/Countries'
-import { useState, useEffect } from 'react'
+import Search from "../components/Search";
+import Countries from "../components/Countries";
+import { useState, useEffect } from "react";
+import { fetchCountries } from "../api/countries";
 
 const HomePage = () => {
+  const [countries, setCountries] = useState([]);
+  const [debouncedInput, setDebouncedInput] = useState("");
+  const [filteredCountries, setFilteredCountries] = useState([]);
+  const [input, setInput] = useState("");
+  const [regions, setRegions] = useState([]);
+  const [filteredSubRegions, setFilteredSubRegions] = useState([]);
+  const [subRegion, setSubRegion] = useState("");
+  const [region, setRegion] = useState("");
 
-    const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    const getCountries = async () => {
+      try {
+        const { data, regions } = await fetchCountries();
 
-    useEffect(() => {
-        const fetchCountries = async () => {
-            try {
-                const res = await fetch(
-                    "https://restcountries.com/v3.1/all?fields=name,flags,cca3,region,capital,population"
-                );
+        setCountries(data);
+        setFilteredCountries(data);
+        setRegions(regions);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-                if (!res.ok) {
-                    throw new Error("Failed to fetch countries");
-                }
+    getCountries();
+  }, []);
 
-                const data = await res.json();
-                console.log(data);
-                setCountries(data);
-            } catch (err) {
-                console.error(err);
-            }
-        };
+  //for debouncing
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedInput(input);
+    }, 400);
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [input]);
 
-        fetchCountries();
-    }, []);
+  useEffect(() => {
+    if (region) {
+      const subs = [
+        ...new Set(
+          countries
+            .filter((c) => c.region?.toLowerCase() === region.toLowerCase())
+            .map((c) => c.subregion)
+        ),
+      ];
+      setFilteredSubRegions(subs);
+      setSubRegion("");
+    } else {
+      setFilteredSubRegions([]);
+      setSubRegion("");
+    }
+  }, [region, countries]);
+
+  useEffect(() => {
+    let filtered = countries;
+
+    if (debouncedInput.trim() !== "") {
+      filtered = countries.filter((country) =>
+        country.name.common.toLowerCase().startsWith(input.toLowerCase())
+      );
+    }
+
+    if (region !== "") {
+      filtered = filtered.filter(
+        (country) => country.region.toLowerCase() === region.toLowerCase()
+      );
+    }
+
+    if (subRegion !== "") {
+      filtered = filtered.filter(
+        (country) => country.subregion.toLowerCase() === subRegion.toLowerCase()
+      );
+    }
+
+    setFilteredCountries(filtered);
+  }, [debouncedInput, region, subRegion]);
+
+  if (filteredCountries.length === 0 && input === "") {
     return (
-        <>
-            <Search />
-            <Countries countries={countries} />
-        </>
-    )
-}
+      <div className="p-10 text-center text-gray-500 dark:text-gray-300">
+        Loading country details...
+      </div>
+    );
+  }
 
-export default HomePage
+  return (
+    <div className="max-w-[95%] mx-auto px-6">
+      <Search
+        input={input}
+        setInput={setInput}
+        regions={regions}
+        region={region}
+        setRegion={setRegion}
+        subRegions={filteredSubRegions}
+        subRegion={subRegion}
+        setSubRegion={setSubRegion}
+      />
+
+      {filteredCountries.length <= 0 && input !== "" ? (
+        <div className="text-center py-10 text-lg font-semibold text-gray-700 dark:text-gray-300">
+          No country found "<span className="text-blue-500">{input}</span>"
+        </div>
+      ) : (
+        <Countries countries={filteredCountries} />
+      )}
+    </div>
+  );
+};
+
+export default HomePage;
